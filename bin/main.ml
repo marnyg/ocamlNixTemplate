4e2e9d621d87526cef315d3389dc9a40e0794498
marius <marnyg@proton.me>
2023-10-28 16:10:07 +0000

    basic setup

--- bin/main.ml
+++ bin/main.ml
@@ -1,12 +1,2 @@
 let () = print_endline "Hello, World!"
 
-#open Core
-(**)
-(* let () = *)
-(*   let%test "addition test" = *)
-(*     Int.( = ) (1 + 1) 2 *)
-(*   in *)
-(*   let%test "subtraction test" = *)
-(*     Int.( = ) (3 - 1) 2 *)
-(*   in *)
-(*   () *)
--- flake.nix
+++ flake.nix
@@ -8,44 +8,40 @@
         ocaml
         ocamlPackages.dune_3
         opam
-        opam2nix.packages.x86_64-linux.opam2nix # Adding opam2nix to the shell path
       ];
-      #] ++ builtins.trace "OPAM selection: ${builtins.toJSON (import ./opam-selection.nix {}).selection}";
-      #] ++ builtins.attrValues (import ./opam-selection.nix {}).selection;  # Importing from local directory
-      #] ++ builtins.attrValues (import ./opam-selection.nix {}).selection;  # Importing from local directory
       shellHook = '' 
         export NIX_PATH=nixpkgs=${nixpkgs.outPath}  # set NIX_PATH
-        #opam2nix resolve myPro.opam
+        opam switch create my_switch 4.12.0 || opam switch my_switch
         eval $(opam env)
+        #opam install . --deps-only
       '';
 
     };
 
     packages.x86_64-linux.myOcamlPackage = with nixpkgs.legacyPackages.x86_64-linux; ocamlPackages.buildDunePackage rec {
-            pname = "myPro";
-            version = "0.1.0";
-            duneVersion = "3";
+      pname = "myPro";
+      version = "0.1.0";
+      duneVersion = "3";
       src = ./.;
 
-            buildInputs = [
-            ocamlPackages.core
-                # Ocaml package dependencies needed to build go here.
-      #buildInputs = (import ./nix {}).selections;  # Importing from local directory
-            ];
+      buildInputs = [
+        ocamlPackages.core
+      ];
 
-            strictDeps = true;
+      strictDeps = true;
 
       doCheck = true;
       checkTarget = "test";
 
-            preBuild = ''
-              dune build myPro.opam
-            '';
+      #preBuild = ''
+      #  dune build myPro.opam
+      #'';
 
-      
+
     };
     packages.x86_64-linux.default = packages.x86_64-linux.myOcamlPackage;
     app.x86_64-linux.default = packages.x86_64-linux.myOcamlPackage;
+    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
   };
 }
 
