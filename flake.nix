1a7072f8909035dd61b245fa0d50542d73d5cb74
marius <marnyg@proton.me>
2023-10-29 14:16:37 +0000

    cleanup

--- flake.nix
+++ flake.nix
@@ -3,19 +3,17 @@
 
   inputs.opam2nix.url = "github:dwarfmaster/opam2nix";
   outputs = { self, nixpkgs, opam2nix }: rec {
-    devShell.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux; mkShell {
+    devShells.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux; mkShell {
       buildInputs = [
         ocaml
         ocamlPackages.dune_3
         opam
       ];
       shellHook = '' 
-        export NIX_PATH=nixpkgs=${nixpkgs.outPath}  # set NIX_PATH
         opam switch create my_switch 4.12.0 || opam switch my_switch
         eval $(opam env)
         #opam install . --deps-only
       '';
-
     };
 
     packages.x86_64-linux.myOcamlPackage = with nixpkgs.legacyPackages.x86_64-linux; ocamlPackages.buildDunePackage rec {
@@ -29,22 +27,17 @@
         ocamlPackages.alcotest
         ocamlPackages.ppx_inline_test
       ];
-
       strictDeps = true;
 
-
-    useDuneConfig = true;  # Use this to read dependencies from .opam file
+      useDuneConfig = true; # Use this to read dependencies from .opam file
       doCheck = true;
       checkTarget = "test";
 
       #preBuild = ''
       #  dune build myPro.opam
       #'';
-
-
     };
     packages.x86_64-linux.default = packages.x86_64-linux.myOcamlPackage;
-    app.x86_64-linux.default = packages.x86_64-linux.myOcamlPackage;
     formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
   };
 }
